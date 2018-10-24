3aee3bfbc62a7813914efe8af454c4ed8b93b735
Dileep Rajput <dileep_rajput@yahoo.com>
2018-10-23 20:43:53 -0400

    Add method to append svg container to body

--- barchart/index.js
+++ barchart/index.js
@@ -1,1 +1,22 @@
-console.log("working");
+
+document.addEventListener("DOMContentLoaded", function(event) {
+	const height = 100;
+	const width = 500;
+
+	createSvg(height, width);
+});
+
+
+/**
+ * append SVG container for graph
+ *
+ * @param int h, height of svg
+ * @param int w, width of svg
+ * @return nothing
+ */
+function createSvg(h, w) {
+	d3.select("body")
+	  .append("svg")
+	  .attr("width", w)
+	  .attr("height", h);
+}
