6b91a6f667ef66d7e8230392dc8c1ac5e27d1c69
Dileep Rajput <dileep_rajput@yahoo.com>
2018-10-23 21:05:40 -0400

    Get barchart data and use it to rendera barchart

--- barchart/index.js
+++ barchart/index.js
@@ -2,10 +2,34 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 	const height = 100;
 	const width = 500;
+	const dataSource = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json";
 
-	createSvg(height, width);
+	//get GDP data and render a barchart with it
+	fetch(dataSource).then(function(response) {
+		return response.json();	
+	})
+	.then(function(json) {
+		let data = json.data;
+		createBarChart(data, height, width);
+	})
+	.catch(function(err) {
+		console.log(err);
+	});
+
 });
 
+
+/**
+ * create a barchart with the given data and append it to the body tag.
+ *
+ * @param data, an array of data where each element is an array whose first element contains the x value and the second element the y value.
+ * @param height, height of the barchart
+ * @param width, width of the barchart
+ * @return nothing
+ */
+function createBarChart(data, h, w) {
+	createSvg(h, w);
+}
 
 /**
  * append SVG container for graph
@@ -20,3 +44,4 @@ function createSvg(h, w) {
 	  .attr("width", w)
 	  .attr("height", h);
 }
+
