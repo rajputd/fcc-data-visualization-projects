e083792c6ad0318f74e479756bf74bbac7313933
Dileep Rajput <dileep_rajput@yahoo.com>
2018-10-26 21:48:12 -0400

    Render data with Rect elements

--- barchart/index.js
+++ barchart/index.js
@@ -1,12 +1,12 @@
 
 document.addEventListener("DOMContentLoaded", function(event) {
-	const height = 100;
-	const width = 500;
+	const height = 200;
+	const width = 1500;
 	const dataSource = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json";
 
 	//get GDP data and render a barchart with it
 	fetch(dataSource).then(function(response) {
-		return response.json();	
+		return response.json();
 	})
 	.then(function(json) {
 		let data = json.data;
@@ -28,7 +28,25 @@ document.addEventListener("DOMContentLoaded", function(event) {
  * @return nothing
  */
 function createBarChart(data, h, w) {
-	createSvg(h, w);
+	const svg = createSvg(h, w);
+
+	const yScale = d3.scaleLinear()
+									 .domain([0,
+														d3.max(data, function(d) { return d[1] })])
+									 .range([0, h]);
+
+	const barWidth = Math.floor(w / data.length);
+
+	//add rectangles
+	svg.selectAll("rect")
+	   .data(data)
+	   .enter()
+	   .append("rect")
+		 .attr("x", function(d,i) { return i * barWidth })
+		 .attr("y", function(d, i) { return h - yScale(d[1])})
+		 .attr("width", barWidth)
+		 .attr("height", function(d, i) { return yScale(d[1]) });
+
 }
 
 /**
@@ -39,9 +57,8 @@ function createBarChart(data, h, w) {
  * @return nothing
  */
 function createSvg(h, w) {
-	d3.select("body")
-	  .append("svg")
-	  .attr("width", w)
-	  .attr("height", h);
+	return d3.select("body")
+			  .append("svg")
+			  .attr("width", w)
+			  .attr("height", h);
 }
-
